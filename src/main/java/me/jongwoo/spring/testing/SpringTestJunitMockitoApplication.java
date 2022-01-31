93810c10e0602f036a966e3fce938f6f597b35d1
jongwoo <leejongwoo.dev@gmail.com>
2022-01-31 18:11:45 +0900

    init commit & create jpa entity

---
+++ src/main/java/me/jongwoo/spring/testing/SpringTestJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package me.jongwoo.spring.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestJunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jongwoo/spring/testing/SpringTestJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jongwoo.spring.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
