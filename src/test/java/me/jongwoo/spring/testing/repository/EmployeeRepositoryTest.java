7b2a7fdc81685f641787e6954f9c3440880fcd4b
jongwoo <leejongwoo.dev@gmail.com>
2022-01-31 23:46:28 +0900

    Testing for get all employees operation

--- src/test/java/me/jongwoo/spring/testing/repository/EmployeeRepositoryTest.java
+++ src/test/java/me/jongwoo/spring/testing/repository/EmployeeRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,6 +33,34 @@ class EmployeeRepositoryTest {
         //then - verify the output
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0);
+    }
+
+
+    @DisplayName("JUnit test for get all employees operation")
+    @Test
+    void givenEmployeesList_whenFindAll_thenEmployeesList(){
+        //given - precondition ro setup
+        Employee employee = Employee.builder()
+                .firstName("jongwoo")
+                .lastName("lee")
+                .email("jongwoo@email.com")
+                .build();
+
+        Employee employee1 = Employee.builder()
+                .firstName("gildong")
+                .lastName("hong")
+                .email("hong@email.com")
+                .build();
+
+        employeeRepository.save(employee);
+        employeeRepository.save(employee1);
+
+        //when - action or the behaviour that we are going test
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        //then - verify the output
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
     }
 
 }
