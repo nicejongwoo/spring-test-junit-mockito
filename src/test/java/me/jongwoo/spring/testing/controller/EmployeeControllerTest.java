5fcc2a059a6d6eaad0337165cb2d5796323b8b55
jongwoo <leejongwoo.dev@gmail.com>
2022-02-01 22:46:27 +0900

    Testing updateEmployee RestAPI

--- src/test/java/me/jongwoo/spring/testing/controller/EmployeeControllerTest.java
+++ src/test/java/me/jongwoo/spring/testing/controller/EmployeeControllerTest.java
@@ -1,10 +1,12 @@
 package me.jongwoo.spring.testing.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.jongwoo.spring.testing.entity.Employee;
 import me.jongwoo.spring.testing.service.EmployeeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,8 +21,8 @@ import java.util.Optional;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.BDDMockito.willAnswer;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -119,6 +121,68 @@ class EmployeeControllerTest {
 
         //when - action or the behaviour that we are going test
         ResultActions response = mockMvc.perform(get("/api/employees/{id}", id));
+
+        //then - verify the output
+        response.andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+
+    @DisplayName("JUnit test for updateEmployee RestAPI")
+    @Test
+    void givenUpdatedEmployee_whenUpdateEmployee_thenReturnUpdateEmployeeObject() throws Exception {
+        //given - precondition ro setup
+        long id = 1L;
+        Employee savedEmployee = Employee.builder()
+                .id(id)
+                .firstName("jongwoo")
+                .lastName("lee")
+                .email("jongwoo@email.com")
+                .build();
+
+        Employee updatedEmployee = Employee.builder()
+                .id(id)
+                .firstName("jw")
+                .lastName("kim")
+                .email("jw@email.com")
+                .build();
+
+        given(employeeService.getEmployeeById(id)).willReturn(Optional.of(savedEmployee));
+        given(employeeService.updateEmployee(any(Employee.class))).willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        //when - action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        //then - verify the output
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())));
+    }
+
+
+    @DisplayName("JUnit test for updateEmployee RestAPI with invalid employeeId")
+    @Test
+    void givenIfNotExistsEmployee_whenUpdateEmployee_thenReturn404() throws Exception {
+        //given - precondition ro setup
+        long id = 1L;
+        Employee updatedEmployee = Employee.builder()
+                .id(id)
+                .firstName("jw")
+                .lastName("kim")
+                .email("jw@email.com")
+                .build();
+
+        given(employeeService.getEmployeeById(id)).willReturn(Optional.empty());
+        given(employeeService.updateEmployee(any(Employee.class))).willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        //when - action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
 
         //then - verify the output
         response.andDo(print())
