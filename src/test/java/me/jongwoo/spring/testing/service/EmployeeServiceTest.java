2f5334e710b6cc803c5180462b4582e0e431ed79
jongwoo <leejongwoo.dev@gmail.com>
2022-02-01 15:07:38 +0900

    Testing saveEmployee at AlreadyExistException

--- src/test/java/me/jongwoo/spring/testing/service/EmployeeServiceTest.java
+++ src/test/java/me/jongwoo/spring/testing/service/EmployeeServiceTest.java
@@ -1,8 +1,10 @@
 package me.jongwoo.spring.testing.service;
 
 import me.jongwoo.spring.testing.entity.Employee;
+import me.jongwoo.spring.testing.exception.AlreadyExistsException;
 import me.jongwoo.spring.testing.repository.EmployeeRepository;
 import me.jongwoo.spring.testing.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
@@ -52,6 +57,22 @@ class EmployeeServiceTest {
 
         //then - verify the output
         assertThat(savedEmployee).isNotNull();
+    }
+
+
+    @DisplayName("JUnit test for saveEmployee which throws exception")
+    @Test
+    void givenExistingEmail_whenSaveEmployee_thenThrowsException(){
+        //given - precondition ro setup
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.of(employee));
+
+        //when - action or the behaviour that we are going test
+        Assertions.assertThrows(AlreadyExistsException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        //then - verify the output
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 
 }
